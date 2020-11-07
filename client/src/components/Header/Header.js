0927b8693ed114761376010084489ef83978dd5c
Seychelle Koplik <sey.koplik@gmail.com>
2020-11-06 21:37:00 -0500

    adjusted styling

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,12 +1,30 @@
 import React from "react";
+import NavBar from "../NavBar/NavBar";
+import { Container } from 'reactstrap';
+import "./Header.css";
 
-function Header(props) {
-	return (
-		<header>
-			<section className="jumbotron mt-3"><h1>MERN Blog</h1></section>
-			{props.children}
-		</header>
-	);
+const Header = () => {
+    return (
+        <header>
+            
+            <NavBar />
+
+            <Container fluid={true}> 
+            <div className="banner">
+                <div className="row">
+                    <div className="col-4">
+                    <img src="https://openbookphilly.com/wp-content/uploads/2016/11/bookstack.png" />
+                    </div>
+                <section className="col-8 headText">
+                    <h1>GOOGLE BOOK SEARCH</h1>
+                    <h5>Make searches for books you like and save them in your database!</h5>
+                </section>
+                </div>
+                </div>
+            </Container>
+
+        </header>
+    )
 }
 
 export default Header;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,19 +1,23 @@
 import axios from "axios";
 
 export default {
-	getPosts: () => {
-		return axios.get("/api/post");
-	},
-	getPostById: id => {
-		return axios.get("/api/post/" + id)
-	},
-	savePost: newPost => {
-		return axios.post("/api/post/", newPost)
-	},
-	updatePost: (id, updatedPost) => {
-		return axios.put("/api/post/" + id, updatedPost)
-	},
-	removePost: id => {
-		return axios.delete("/api/post/" + id)
-	}
-}
+    // Gets the book with the searched title from Google Books API
+    getBook: function (book) {
+        return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book);
+    },
+
+    // Gets all saved books from our database
+    getBooks: function () {
+        return axios.get("/api/books");
+    },
+
+    // Saves a book to our database
+    saveBook: function (bookData) {
+        return axios.post("/api/books", bookData);
+    },
+
+    // Deletes the book with the given id
+    deleteBook: function (id) {
+        return axios.delete("/api/books/" + id);
+    },
+};
